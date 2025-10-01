c703b14f8157a53d0e5b7bc723b24bfd527ce209
ArkanUbaidillah <arkanwarman@gmail.com>
2025-10-01 22:08:37 +0700

    Initial commit: PWA website for Arkan Ubaidillah Warman

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "webarkt-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./about.html",
+  "./contact.html",
+  "./offline.html",
+  "./manifest.json",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).catch(() => {
+        if (event.request.mode === "navigate") {
+          return caches.match("./offline.html");
+        }
+      });
+    })
+  );
+});
